6b90d59990e09bb2d14e88e0d899151ec3900f5b
yoricktf <yorick.tenfeld@gmail.com>
2018-02-14 08:36:43 +0100

    made external javascript file working

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-// https://maps.googleapis.com/maps/api/staticmap?key=YOUR_API_KEY&center=-36.851066918483525,174.75766600527342&zoom=12&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xebe3cd&style=element:labels.text.fill%7Ccolor:0x523735&style=element:labels.text.stroke%7Ccolor:0xf5f1e6&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0xc9b2a6&style=feature:administrative.land_parcel%7Celement:geometry.stroke%7Ccolor:0xdcd2be&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xae9e90&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x93817c&style=feature:poi.attraction%7Cvisibility:off&style=feature:poi.business%7Cvisibility:on&style=feature:poi.government%7Cvisibility:off&style=feature:poi.medical%7Cvisibility:off&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0xa5b076&style=feature:poi.park%7Celement:labels.text%7Cvisibility:off&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x447530&style=feature:poi.place_of_worship%7Cvisibility:off&style=feature:poi.school%7Cvisibility:off&style=feature:poi.sports_complex%7Cvisibility:off&style=feature:road%7Celement:geometry%7Ccolor:0xf5f1e6&style=feature:road.arterial%7Celement:geometry%7Ccolor:0xfdfcf8&style=feature:road.highway%7Celement:geometry%7Ccolor:0xf8c967&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0xe9bc62&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0xe98d58&style=feature:road.highway.controlled_access%7Celement:geometry.stroke%7Ccolor:0xdb8555&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x806b63&style=feature:transit.line%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:transit.line%7Celement:labels.text.fill%7Ccolor:0x8f7d77&style=feature:transit.line%7Celement:labels.text.stroke%7Ccolor:0xebe3cd&style=feature:transit.station%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:water%7Celement:geometry.fill%7Ccolor:0x92d3d3&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x92998d&size=480x360
+// ======================== NAVBAR STICKY
+window.onscroll = function() {myFunction()};
 
-// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCr9DPw9sQbp95s7nrvqultFeoXTyoygOg&callback=initMap"></script>
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-var styledMapType = new google.maps.StyledMapType
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+// ======================== MAP
   function initMap() {
+// ======================== SHOP LOCATIONS
       var auckland = {lat: -36.848460, lng:174.763332};
       var phils = {lat: -36.916270,  lng:174.636482};
       var map = new google.maps.Map(document.getElementById('map'), {
+       
         zoom: 11,
         center: auckland,
         styles: [
@@ -317,30 +328,15 @@ var styledMapType = new google.maps.StyledMapType
   }
 ]
       });
-
-      var marker = new google.maps.Marker({
-        position: auckland,
-        map: map,
-        title: 'testing'
-      });
-      var marker = new google.maps.Marker({
-        position: phils,
-        map: map
-      });
-      marker.setMap(map);
+// ======================== MARKERS ON MAP
+var marker = new google.maps.Marker({
+  position: auckland,
+  map: map,
+  title: "test"
+});
+var marker = new google.maps.Marker({
+  position: phils,
+  map: map
+});
+marker.setMap(map);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
